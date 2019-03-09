c951b44e1ad131eb04e3cfb620b2bedfedfb8823
Linas Vainoras <linas.vainoras@gmail.com>
2019-03-09 22:02:34 +0000

    added linkedin url

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import './styles/style.scss';
 
 let contactButton = document.getElementById('contactButton');
 let contactTextarea = document.getElementById('contactTextarea');
+let typing = false;
 
 const showContactForm = (event) => {
-    if(event.key !== 'Backspace') {
+    if(!typing && event.key !== 'Backspace') {
+        typing = true;
         contactButton.classList.add('typing');
         contactTextarea.focus();
     }
@@ -13,6 +15,7 @@ const showContactForm = (event) => {
 const handleInput = (event) => {
     if(event.target.value.length === 0 && event.key === 'Backspace') {
         contactButton.classList.remove('typing');
+        typing = false;
     }
 }
 
