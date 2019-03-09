4511340a1f2c5d6aa97907a3457ab112e420b41f
Linas Vainoras <linas.vainoras@gmail.com>
2019-03-09 21:02:58 +0000

    added textarea

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
 import './styles/style.scss';
+
+let contactButton = document.getElementById('contactButton');
+let contactButtonHint = document.getElementById('contactButtonHint');
+let contactTextarea = document.getElementById('contactTextarea');
+let contactButtonHeading = document.getElementById('contactButtonHeading');
+let typing = false;
+
+const showContactForm = (event) => {
+    if(event.key !== 'Backspace') {
+        contactButton.classList.add('typing');
+        contactButtonHint.classList.add('typing');
+        contactTextarea.classList.add('typing');
+        contactButtonHeading.classList.add('typing');
+        contactTextarea.focus();
+    }
+}
+
+const handleInput = (event) => {
+    if(event.target.value.length === 0 && event.key === 'Backspace') {
+        contactButton.classList.remove('typing');
+        contactButtonHint.classList.remove('typing');
+        contactTextarea.classList.remove('typing');
+        contactButtonHeading.classList.remove('typing');
+    }
+}
+
+contactButton.addEventListener('click', showContactForm);
+contactTextarea.addEventListener('keyup', handleInput)
+document.addEventListener('keydown', showContactForm);
+
