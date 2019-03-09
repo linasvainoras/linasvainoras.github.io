76248813b3d5e380089298ea01cf646bfedb7bd1
Linas Vainoras <linas.vainoras@gmail.com>
2019-03-09 21:29:01 +0000

    refactor

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 import './styles/style.scss';
 
 let contactButton = document.getElementById('contactButton');
-let contactButtonHint = document.getElementById('contactButtonHint');
 let contactTextarea = document.getElementById('contactTextarea');
-let contactButtonHeading = document.getElementById('contactButtonHeading');
-let typing = false;
 
 const showContactForm = (event) => {
     if(event.key !== 'Backspace') {
         contactButton.classList.add('typing');
-        contactButtonHint.classList.add('typing');
-        contactTextarea.classList.add('typing');
-        contactButtonHeading.classList.add('typing');
         contactTextarea.focus();
     }
 }
@@ -19,9 +13,6 @@ const showContactForm = (event) => {
 const handleInput = (event) => {
     if(event.target.value.length === 0 && event.key === 'Backspace') {
         contactButton.classList.remove('typing');
-        contactButtonHint.classList.remove('typing');
-        contactTextarea.classList.remove('typing');
-        contactButtonHeading.classList.remove('typing');
     }
 }
 
