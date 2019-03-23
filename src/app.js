92231aed857b8406d4f129e12b1a0786bcb0a9ce
Linas Vainoras <linas.vainoras@gmail.com>
2019-03-23 17:25:28 +0000

    node-contact form

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './styles/style.scss';
 
 let contactButton = document.getElementById('contactButton');
 let contactTextarea = document.getElementById('contactTextarea');
+let contactForm = document.getElementById('contactForm');
 let typing = false;
 
 const showContactForm = (event) => {
@@ -19,7 +20,28 @@ const handleInput = (event) => {
   }
 }
 
+const send = (event) => {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  let formBody = [];
+
+  for (const [key, value] of form.entries()) {
+    formBody.push(`${key}=${value}`);
+  };
+
+  formBody = formBody.join("&");
+
+  fetch('https://polar-tundra-72381.herokuapp.com/contact', {
+    headers: new Headers({ "Content-Type": "application/x-www-form-urlencoded" }),
+    method: 'POST',
+    body: formBody
+  })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+}
+
 contactButton.addEventListener('click', showContactForm);
-contactTextarea.addEventListener('keyup', handleInput)
+contactTextarea.addEventListener('keyup', handleInput);
 document.addEventListener('keydown', showContactForm);
+contactForm.addEventListener('submit', send);
 
